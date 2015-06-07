e332daa990dd7169ce65cd49ea0b12b835ab2072
Takuya BAN <do38gkma@gmail.com>
2015-06-07 19:17:45 +0900

    destroy_clusterの修正

--- server/fabfile.py
+++ server/fabfile.py
@@ -1,14 +1,15 @@
-#coding: utf-8
-from fabric.api import run,env,sudo
-
+# coding: utf-8
+from fabric.api import run, env
 #env.hosts = ["157.82.3.143"]
 env.user = 'root'
 env.key_filename = '/home/id_rsa'
+
 
 def sample():
     run("stop-all.sh")
     run("start-all.sh")
     run("hadoop jar /home/hadoop/hadoop-2.6.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.0.jar pi 100 100")
+
 
 def get_lxc_name(ip, is_master):
     """
@@ -16,11 +17,13 @@ def get_lxc_name(ip, is_master):
     """
     return ('master-' if is_master else 'slave-') + ip[-2:]
 
+
 def set_host_string(ip_or_lxc_name):
     """
     ipアドレスまたはコンテナ名に応じてfabric.api.env.host_stringを再設定.
     """
     env.host_string = ("157.82.3.140" if int(ip_or_lxc_name[-1]) % 2 == 0 else "157.82.3.141")
+
 
 def remote_run(ip_or_lxc_name, command):
     """
@@ -44,6 +47,7 @@ def make_lxc(ip, is_master):
     command = 'cat {0} | sed s/IP_ADDRESS/{1}/g > {2}' \
       .format(interfaces_templace, ip, interfaces_file)
     remote_run(ip, command)
+
 
 def create_cluster(master, slave_list):
 
@@ -110,20 +114,14 @@ def create_cluster(master, slave_list):
     run('start-dfs.sh')
     run('start-yarn.sh')
 
-def make_master(ip, slave_list):
-    pass
 
-def make_slave(ip):
-    pass
-
-def destroy_cluster(master,slave_list):
-	
-    master_name ="master"+ master[-1].zfill(2)
-    run("lxc-destroy -n %s" %master_name)
+def destroy_cluster(master, slave_list):
+    master_name = get_lxc_name(master, is_master=True)
+    run("lxc-destroy -n " + master_name)
     
     for slave_ip in slave_list:
-    	slave_name ="slave"+ slave_ip[-1].zfill(2)
-    	run("lxc-destroy -n %s" % slave_name)
+        slave_name = get_lxc_name(slave_ip, is_master=False)
+        run("lxc-destroy -n " + slave_name)
 
 
 if __name__ == '__main__':
