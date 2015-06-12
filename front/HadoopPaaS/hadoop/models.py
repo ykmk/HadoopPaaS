f7a1cea4dab5857bbd7fc1de7abc4e19cf62d3d1
Takuya BAN <do38gkma@gmail.com>
2015-06-12 20:40:11 +0900

    models.pyを修正(マスタ情報を入れ忘れていたので)

--- front/HadoopPaaS/hadoop/models.py
+++ front/HadoopPaaS/hadoop/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Cluster(models.Model):
     user = models.OneToOneField(User) #クラスタ作成者
+    master = models.OneToOneField(Node) #クラスタのマスタノード
     name = models.CharField(max_length=50) #クラスタ名
     release_date = models.DateField() #クラスタ作成日時
 
