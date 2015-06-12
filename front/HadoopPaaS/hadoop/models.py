bc4345fc10d5cee1934976b5b980f2d1a2cf516d
Takuya BAN <do38gkma@gmail.com>
2015-06-12 19:51:30 +0900

    HadoopPaaSのフロントのmodels.pyを弄った

---
+++ front/HadoopPaaS/hadoop/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Cluster(models.Model):
+    user = models.OneToOneField(User) #クラスタ作成者
+    name = models.CharField(max_length=50) #クラスタ名
+    release_date = models.DateField() #クラスタ作成日時
+
+
+class Node(models.Model):
+    cluster = models.ForeignKey(Cluster) #ノードの所属するクラスタ
+    ip = models.CharField(max_length=15) #IPアドレス
